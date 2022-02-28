faa165e53d6267a3b952993e5f21efa93c618d58
Matthias Dumke <matthias.dumke@gmx.net>
2022-02-28 19:46:10 +0100

    feat: introduce character stats

--- src/states/play-state.js
+++ src/states/play-state.js
@@ -6,16 +6,20 @@ import { Player } from '../player.js'
 class PlayState {
   constructor () {
     this.hiragana = new Hiragana()
+    this.stats = this.prepareStats(this.hiragana.chars)
+    this.target = null
     this.hero = null
     this.enemy = null
+    this.startTime = null
+    this.interval = null
   }
 
   enter () {
     display.show('play-screen')
     display.showWallpaper()
     display.focusInput()
-    this.hero = new Player(3, display.getHeroPowerbar())
-    this.enemy = new Player(3, display.getEnemyPowerbar())
+    this.hero = new Player(10, display.getHeroPowerbar())
+    this.enemy = new Player(10, display.getEnemyPowerbar())
     this.setInputListener()
     this.reset()
   }
@@ -27,12 +31,15 @@ class PlayState {
 
   reset () {
     this.target = this.hiragana.sample()
+    this.startTime = performance.now()
+    this.interval = setInterval(() => this.handleTimeout(), 2000)
     display.showTarget(this.target.hiragana)
     display.unmarkError()
     display.clearInput()
   }
 
   gameOver () {
+    clearInterval(this.interval)
     console.log('thats it for you buddy...')
   }
 
@@ -41,8 +48,14 @@ class PlayState {
   }
 
   async handleSuccess () {
+    clearInterval(this.interval)
     display.clearInput()
     display.unmarkError()
+    this.updateStatsWithSuccess(
+      this.target.romaji,
+      performance.now() - this.startTime
+    )
+    console.log(this.stats)
     await audio.playVoiceRecording(this.target.romaji)
     this.enemy.takeHit()
     if (this.enemy.isDefeated()) {
@@ -55,6 +68,7 @@ class PlayState {
   handleError () {
     display.markError()
     display.clearInput()
+    this.updateStatsWithError(this.target.romaji)
     this.hero.takeHit()
     if (this.hero.isDefeated()) {
       this.gameOver()
@@ -63,6 +77,13 @@ class PlayState {
 
   handleWait () {
     display.unmarkError()
+  }
+
+  handleTimeout () {
+    this.hero.takeHit()
+    if (this.hero.isDefeated()) {
+      this.gameOver()
+    }
   }
 
   step () {
@@ -86,6 +107,44 @@ class PlayState {
     document
       .querySelector('#input')
       .addEventListener('input', this.step.bind(this))
+  }
+
+  prepareStats (hiragana) {
+    return hiragana
+      .map(([, romaji]) => romaji)
+      .reduce(
+        (memo, chars) => ({
+          ...memo,
+          [chars]: {
+            mean: null,
+            n: 0,
+            errors: 0
+          }
+        }),
+        {}
+      )
+  }
+
+  updateStatsWithSuccess (char, dt) {
+    const { mean, n, errors } = this.stats[char]
+
+    if (n === null) {
+      this.stats[char] = {
+        mean: dt,
+        n: 1,
+        errors
+      }
+    } else {
+      this.stats[char] = {
+        mean: (mean * n) / (n + 1) + dt / (n + 1),
+        n: n + 1,
+        errors
+      }
+    }
+  }
+
+  updateStatsWithError (char) {
+    this.stats[char].errors++
   }
 }
 
